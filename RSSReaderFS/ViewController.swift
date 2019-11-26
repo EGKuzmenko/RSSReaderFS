b262443ceb8962bb0b295887e7fd36f2425a816a
g.kuzm4 <job.kuzm@yandex.ru>
2019-11-26 21:12:58 +0700

    add major files

---
+++ RSSReaderFS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSReaderFS
+//
+//  Created by Георгий Кузьменко on 25/11/2019.
+//  Copyright © 2019 Georgy Kuzmenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
