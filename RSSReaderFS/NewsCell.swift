e0a3f3df2c0955dd6027576afd9ca90af267b98a
g.kuzm4 <job.kuzm@yandex.ru>
2019-11-30 10:28:46 +0700

    add NewsCell

---
+++ RSSReaderFS/NewsCell.swift
@@ -0,0 +1,19 @@
+//
+//  NewsCell.swift
+//  RSSReaderFS
+//
+//  Created by Георгий Кузьменко on 28/11/2019.
+//  Copyright © 2019 Georgy Kuzmenko. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var newsLabel: UILabel!
+    
+    func configureWith(article: Data) {
+        
+    }
+}
