d3f4e7c757b7feb301817a1118e2367e18d4ba86
g.kuzm4 <job.kuzm@yandex.ru>
2019-12-06 16:17:01 +0700

    add User

---
+++ RSSReaderFS/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  RSSReaderFS
+//
+//  Created by Георгий Кузьменко on 06/12/2019.
+//  Copyright © 2019 Georgy Kuzmenko. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+}
