a64f0c7f22181de2777c8377e43c97a45b8c4224
g.kuzm4 <job.kuzm@yandex.ru>
2019-12-07 13:45:33 +0700

    add ILoginView

---
+++ RSSReaderFS/Views/ILoginView.swift
@@ -0,0 +1,13 @@
+//
+//  ILoginView.swift
+//  RSSReaderFS
+//
+//  Created by Георгий Кузьменко on 07/12/2019.
+//  Copyright © 2019 Georgy Kuzmenko. All rights reserved.
+//
+
+import Foundation
+
+protocol ILoginView: AnyObject {
+    func setupInitialState()
+}
