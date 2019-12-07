f07cfe6d8cbae7385c18976763edb7d152602ebc
g.kuzm4 <job.kuzm@yandex.ru>
2019-12-07 10:49:13 +0700

    add LoginViewController

---
+++ RSSReaderFS/Views/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  RSSReaderFS
+//
+//  Created by Георгий Кузьменко on 07/12/2019.
+//  Copyright © 2019 Georgy Kuzmenko. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func onLoginButtonTap(_ sender: UIButton) {
+        
+    }
+    @IBAction func onSigninButtonTap(_ sender: UIButton) {
+        
+    }
+}
