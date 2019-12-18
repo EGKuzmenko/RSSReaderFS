b3429850a7dc3ae81caff3dcb18612b34e8e2edd
g.kuzm4 <job.kuzm@yandex.ru>
2019-12-18 23:47:07 +0700

    add ArticlesParams

---
+++ RSSReaderFS/Units/Feed/ArticlesParams.swift
@@ -0,0 +1,14 @@
+//
+//  ArticlesParams.swift
+//  RSSReaderFS
+//
+//  Created by Георгий Кузьменко on 17/12/2019.
+//  Copyright © 2019 Georgy Kuzmenko. All rights reserved.
+//
+
+import Foundation
+
+struct ArticlesParams {
+    let theme = "popularity"
+    var search: String?
+}
